bf542e9b87885afe2464a3af223e09932727582f
manozchhetri <156447784+manozchhetri@users.noreply.github.com>
2025-05-30 16:00:26 -0400

    second commit
    asking for the user input

--- LAB02/Lab02.js
+++ LAB02/Lab02.js
@@ -3,3 +3,15 @@ const prompt = require('prompt');
 
 // Start the prompt
 prompt.start();
+
+// Ask user to enter a value (R, P, S, full word, or number)
+prompt.get(['userInput'], function (err, result) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Read and clean the user input
+  const inputRaw = result.userInput.toString().trim().toUpperCase();
+  let userSelection = '';
+});
